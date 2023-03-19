dac402abcbe94176a157f09503daa7d1c05ed30f
rahilansari261 <rahilansari261@gmail.com>
2023-03-19 21:54:54 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,144 @@
+import React from 'react'
+import * as Icon from 'react-feather'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <header>
+        <div className='logo'>
+          <Icon.Zap />
+          <span className='logo-name'>Paperweight</span>
+        </div>
+        <div className='user-wrapper'>
+          <div className='username'>Rahil Ansari</div>
+          <div className='user-icon'>
+          <Icon.User />
+          </div>
+        </div>
+      </header>
+      <div className='container'>
+        <aside className='sidebar'>
+          <nav>
+            <ul>
+              <li className='item-flex'>
+                <div>Dashboard</div>
+                <div>
+                  <Icon.Activity />
+                </div>
+              </li>
+              <li className='item-flex'>
+                <div>Clients</div>
+                <div>
+                  <Icon.Users />
+                </div>
+              </li>
+              <li className='item-flex'>
+                <div>Products</div>
+                <div>
+                  <Icon.ShoppingBag />
+                </div>
+              </li>
+              <li className='item-flex'>
+                <div>Invoices</div>
+                <div>
+                  <Icon.Book />
+                </div>
+              </li>
+              <li className='item-flex'>
+                <div>Settings</div>
+                <div>
+                  <Icon.Settings />
+                </div>
+              </li>
+              <li className='item-flex'>
+                <div>Logout</div>
+                <div>
+                  <Icon.LogOut />
+                </div>
+              </li>
+            </ul>
+          </nav>
+        </aside>
+        <main className='main'>
+          <div className='main-wrapper'>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+            Repudiandae accusantium molestiae, placeat veniam eum architecto
+            obcaecati labore debitis temporibus voluptatem est asperiores saepe
+            necessitatibus quo facilis ab dolorem dicta nesciunt atque magni
+            deserunt quibusdam. Maiores a doloribus molestias aspernatur quae
+            inventore aliquam, iste quidem id libero ipsam repudiandae sunt?
+            deleniti eligendi quidem numquam fuga alias soluta. Laudantium nam,
+            necessitatibus neque et vero modi officiis quis nobis fuga beatae
+            fugiat minus earum blanditiis ipsum. Inventore cumque atque a quas
+            fugiat eligendi, perferendis sit, asperiores rerum ex unde quasi
+            quisquam. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+            Maiores beatae incidunt totam dicta perferendis voluptatibus ad
+            libero id impedit, veritatis culpa aliquid deserunt ab hic
+            aspernatur assumenda et! Consectetur, totam. Lorem ipsum dolor sit
+            amet consectetur adipisicing elit. Blanditiis molestiae fugiat
+            dolore, ex sint voluptatem id modi, nemo sed ducimus fuga eius
+            minima doloribus. Asperiores, maiores consequuntur earum maxime
+            soluta molestias corrupti molestiae dignissimos quasi! Pariatur
+            praesentium omnis, ratione quasi iste excepturi quibusdam doloremque
+            ad esse numquam nulla magni minima quas dolorem, unde voluptatibus,
+            facere soluta? Doloremque corporis, aliquid minima eos laboriosam
+            natus ad, vero nisi beatae possimus quasi accusantium distinctio
+            accusamus quos necessitatibus nemo maiores consequuntur quam
+            cupiditate rerum impedit molestiae. Quos deserunt exercitationem
+            atque repellendus alias omnis quas optio rerum doloribus nulla
+            ullam, doloremque rem quod similique voluptatibus. Eligendi
+            provident sequi reiciendis obcaecati pariatur nemo reprehenderit
+            eius recusandae, quos doloremque est fuga quasi sapiente quo facilis
+            expedita rem quas sit ullam dolorem iure qui similique dolore.
+            Soluta perferendis ratione quae exercitationem inventore obcaecati
+            accusantium at quasi amet necessitatibus dolorum officia illum
+            eveniet, laboriosam eum alias optio vero, quas omnis expedita
+            aspernatur enim? Qui aliquid eaque, nostrum officia facere
+            architecto accusamus et non quas unde a, id neque tempora est
+            corrupti expedita minus itaque, enim atque voluptatum nisi quidem!
+            Iste delectus voluptates veritatis quos temporibus, soluta
+            recusandae laboriosam dolorem ullam quaerat sint a qui tempore
+            aspernatur provident! Obcaecati saepe quidem vel est iusto cum ipsam
+            repudiandae aspernatur neque nam dolorem, ipsa architecto animi
+            excepturi, soluta tempore quia, beatae facilis quos et dicta tempora
+            assumenda? Ea commodi eveniet magnam accusamus architecto, at in
+            blanditiis alias dolores laborum hic officiis saepe harum eius
+            praesentium cupiditate quos corporis reiciendis quidem asperiores
+            est libero soluta maxime. Consectetur culpa cum consequuntur iusto
+            tempore hic, veritatis error veniam est. Quisquam saepe omnis eum
+            temporibus illo earum neque nulla ipsam rem. Eligendi amet, placeat
+            quasi accusantium iste, et quae repudiandae laudantium tempora
+            eveniet molestias, laboriosam excepturi ipsam doloremque commodi
+            expedita! Tempore in, alias veniam repellat fugit ut non corrupti
+            dolore rerum eos quisquam, nihil molestias nobis. Commodi non quasi
+            id sed, omnis dolorum cumque! Recusandae, minima iste, nihil unde
+            explicabo repudiandae neque aut quasi, atque nesciunt blanditiis
+            commodi culpa inventore voluptatem quas. Incidunt aspernatur ipsam
+            dicta non itaque optio culpa? Ipsa, accusamus. Sequi tempora harum
+            ea molestias ratione aliquid illum, cupiditate unde velit voluptatem
+            totam laborum ab ipsa expedita iusto quia ut ullam dolorum, nostrum
+            consequuntur natus rem, reprehenderit molestiae veritatis. Commodi
+            blanditiis debitis sunt, vero provident dolores dolor distinctio
+            nostrum natus pariatur iusto reprehenderit inventore delectus
+            veritatis nihil minus voluptatum minima facere quibusdam neque vitae
+            corrupti quasi. Magni placeat ut distinctio esse ad, aperiam, velit
+            veritatis dolore expedita aut consequatur laudantium nesciunt in sed
+            quaerat iste. Quia praesentium rem illum? In, officiis omnis beatae
+            architecto consequatur consectetur libero, possimus atque doloribus,
+            tempora provident recusandae. Dolor consectetur culpa explicabo
+            ipsum placeat ad architecto id assumenda esse. Repudiandae quis rem
+            sed aperiam quae in, fuga aut animi! Dolorum nihil obcaecati iusto,
+            ratione quaerat harum doloremque optio eveniet exercitationem modi
+            officiis rem, corrupti ullam? Culpa consequuntur, rem voluptate
+            temporibus est molestiae perferendis error aut exercitationem rerum
+            reiciendis, magnam sit repellat pariatur voluptatibus doloribus
+            magni ipsum perspiciatis et corrupti.
+          </div>
+        </main>
+      </div>
+    </>
+  )
+}
+
+export default App
