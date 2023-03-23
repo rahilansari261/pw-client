2635fe8080b793f651298dc9e411286949320bc9
rahilansari261 <rahilansari261@gmail.com>
2023-03-23 17:20:16 +0530

    add client page added

--- src/pages/Clients/AddClient.jsx
+++ src/pages/Clients/AddClient.jsx
@@ -12,7 +12,95 @@ export const AddClient = () => {
           <Title>Add new client</Title>
         </TitleWrapper>
       </TitleSection>
-      <DetailSection>All input boxes will appear here...</DetailSection>
+      <DetailSection>
+        <Form>
+          <FormElement>
+            <Label htmlFor=''>Company Name *</Label>
+            <Input
+              type='text'
+              name='company'
+              id='company'
+              autoComplete='off'
+              placeholder='Name of Company to be used in invoices'
+            />
+          </FormElement>
+          <FormElement>
+            <Label htmlFor=''>Client Name *</Label>
+            <Input
+              type='text'
+              name='client_name'
+              id='client_name'
+              autoComplete='off'
+              placeholder=''
+            />
+          </FormElement>
+          <FormElement>
+            <Label htmlFor=''>Client Address *</Label>
+            <TextArea
+              type='text'
+              name='address'
+              id='address'
+              autoComplete='off'
+              placeholder=''
+              rows='2'
+            />
+          </FormElement>
+          <FormElement>
+            <Label htmlFor=''>Phone *</Label>
+            <Input
+              type='number'
+              name='phone'
+              id='phone'
+              autoComplete='off'
+              placeholder=''
+            />
+          </FormElement>
+          <FormElement>
+            <Label htmlFor=''>Email *</Label>
+            <Input
+              type='email'
+              name='email'
+              id='email'
+              autoComplete='off'
+              placeholder=''
+            />
+          </FormElement>
+          <FormElement>
+            <Label htmlFor=''>GST No. *</Label>
+            <Input
+              type='text'
+              name='gst'
+              id='gst'
+              autoComplete='off'
+              placeholder=''
+            />
+          </FormElement>
+          <FormElement>
+            <Label htmlFor=''>Sevice Tax No. *</Label>
+            <Input
+              type='text'
+              name='tax'
+              id='tax'
+              autoComplete='off'
+              placeholder=''
+            />
+          </FormElement>
+          <FormElement>
+            <Label htmlFor=''>Client Notes </Label>
+            <TextArea
+              type='text'
+              name='notes'
+              id='notes'
+              autoComplete='off'
+              placeholder=''
+              rows='2'
+            />
+          </FormElement>
+        </Form>
+          <FormElement>
+            <Button label='success'>Save</Button>
+          </FormElement>
+      </DetailSection>
     </Main>
   )
 }
@@ -32,14 +120,6 @@ const TitleSection = styled.div`
   align-items: center;
   min-height: 60px;
 `
-const DetailSection = styled.div`
-  background-color: var(--white-color);
-  padding: 1em;
-  border-radius: 0 0 4px 4px;
-  max-width: 1000px;
-  overflow-x: auto;
-  margin: 0 auto;
-`
 
 const Title = styled.div`
   padding-left: 8px;
@@ -49,3 +129,54 @@ const Title = styled.div`
 const TitleWrapper = styled.div`
   display: flex;
 `
+const DetailSection = styled.div`
+  background-color: var(--white-color);
+  padding: 1em;
+  border-radius: 0 0 4px 4px;
+  max-width: 1000px;
+  overflow-x: auto;
+  margin: 0 auto;
+`
+const Input = styled.input`
+  background-color: var(--white-color);
+  padding: 8px;
+  color: var(--black-color);
+  border: 1px solid var(--table-border-color);
+  border-radius: 4px;
+  width: 40%;
+  outline: none;
+  font-family: inherit;
+  &:focus {
+    box-shadow: var(--input-bs);
+  }
+`
+
+const TextArea = styled.textarea`
+  background-color: var(--white-color);
+  padding: 8px;
+  color: var(--black-color);
+  border: 1px solid var(--table-border-color);
+  border-radius: 4px;
+  width: 40%;
+  outline: none;
+  font-family: inherit;
+  &:focus {
+    box-shadow: var(--input-bs);
+  }
+`
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const FormElement = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  gap: 2em;
+  margin-bottom: 1em;
+  min-width: 500px;
+  
+`
+const Label = styled.label``
