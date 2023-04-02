a51230040eac433239d723d25c260149cf12be25
rahilansari261 <rahilansari261@gmail.com>
2023-04-02 20:14:25 +0530

    multipage implemented in settings page

---
+++ src/pages/Settings/ChangePassword.jsx
@@ -0,0 +1,133 @@
+import styled from 'styled-components'
+import { Button } from '../../components/Button'
+
+export const ChangePassword = () => {
+  return (
+    <>
+      <Form>
+        <FormElement>
+          <Label htmlFor=''>Company Name </Label>
+          <div style={{ flex: 2 }}> Password</div>
+        </FormElement>
+        <FormElement>
+          <Label htmlFor=''>Name *</Label>
+          <Input
+            type='text'
+            name='name'
+            id='name'
+            autoComplete='off'
+            placeholder=''
+          />
+        </FormElement>
+        <FormElement>
+          <Label htmlFor=''>GST No. *</Label>
+          <Input
+            type='text'
+            name='gst'
+            id='gst'
+            autoComplete='off'
+            placeholder=''
+          />
+        </FormElement>
+        <FormElement>
+          <Label htmlFor=''>Sevice Tax No. *</Label>
+          <Input
+            type='text'
+            name='tax'
+            id='tax'
+            autoComplete='off'
+            placeholder=''
+          />
+        </FormElement>
+
+        <FormElement>
+          <Label htmlFor=''>Phone *</Label>
+          <Input
+            type='number'
+            name='phone'
+            id='phone'
+            autoComplete='off'
+            placeholder=''
+          />
+        </FormElement>
+
+        <FormElement>
+          <Label htmlFor=''> Address *</Label>
+          <TextArea
+            type='text'
+            name='address'
+            id='address'
+            autoComplete='off'
+            placeholder=''
+            rows='2'
+          />
+        </FormElement>
+        <FormElement>
+          <Label htmlFor=''> </Label>
+          <FootNote>
+            Please note these information will be used in invoices. <br />{' '}
+            Please check before saving them.
+          </FootNote>
+        </FormElement>
+      </Form>
+
+      <FormElement>
+        <Button label='success' clickHandle={() => clickHandle('')}>
+          Save My Profile
+        </Button>
+      </FormElement>
+    </>
+  )
+}
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+const FormElement = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  /* gap: 2em; */
+  margin-bottom: 1em;
+  min-width: 500px;
+`
+
+const Label = styled.label`
+  flex: 1;
+`
+const Input = styled.input`
+  background-color: var(--white-color);
+  padding: 8px;
+  color: var(--black-color);
+  border: 1px solid var(--table-border-color);
+  border-radius: 4px;
+  width: 40%;
+  outline: none;
+  font-family: inherit;
+  &:focus {
+    box-shadow: var(--input-bs);
+  }
+  flex: 2;
+`
+
+const TextArea = styled.textarea`
+  background-color: var(--white-color);
+  padding: 8px;
+  color: var(--black-color);
+  border: 1px solid var(--table-border-color);
+  border-radius: 4px;
+  width: 40%;
+  outline: none;
+  font-family: inherit;
+  &:focus {
+    box-shadow: var(--input-bs);
+  }
+  flex: 2;
+`
+const FootNote = styled.div`
+  flex: 2;
+  font-size: 12px;
+`
--- src/pages/Settings/Settings.jsx
+++ src/pages/Settings/Settings.jsx
@@ -3,16 +3,20 @@ import { faGears } from '@fortawesome/free-solid-svg-icons'
 import styled from 'styled-components'
 import { Button } from '../../components/Button'
 import { useState } from 'react'
+import { Profile } from './Profile'
+import { ChangePassword } from './ChangePassword'
+import { Subscription } from './Subscription'
+import { TaxAndTerms } from './TaxAndTerms'
 const settingInitObj = {
   profile: 'info',
-  settings: 'info',
+  taxes: 'info',
   password: 'info',
   subscription: 'info',
 }
 export const Settings = () => {
   const [settingsObj, setSettingsObj] = useState({
     profile: 'primary',
-    settings: 'info',
+    taxes: 'info',
     password: 'info',
     subscription: 'info',
   })
@@ -36,10 +40,10 @@ export const Settings = () => {
             Profile
           </Button>
           <Button
-            label={settingsObj.settings}
-            clickHandle={() => clickHandle('settings')}
+            label={settingsObj.taxes}
+            clickHandle={() => clickHandle('taxes')}
           >
-            Settings
+            Taxes
           </Button>
           <Button
             label={settingsObj.password}
@@ -56,78 +60,25 @@ export const Settings = () => {
         </ButtonWrapper>
       </TitleSection>
       <DetailSection>
-        <Form>
-          <FormElement>
-            <Label htmlFor=''>Company Name </Label>
-            <div style={{ flex: 2 }}>Subuyan Enterprises</div>
-          </FormElement>
-          <FormElement>
-            <Label htmlFor=''>Name *</Label>
-            <Input
-              type='text'
-              name='name'
-              id='name'
-              autoComplete='off'
-              placeholder=''
-            />
-          </FormElement>
-          <FormElement>
-            <Label htmlFor=''>GST No. *</Label>
-            <Input
-              type='text'
-              name='gst'
-              id='gst'
-              autoComplete='off'
-              placeholder=''
-            />
-          </FormElement>
-          <FormElement>
-            <Label htmlFor=''>Sevice Tax No. *</Label>
-            <Input
-              type='text'
-              name='tax'
-              id='tax'
-              autoComplete='off'
-              placeholder=''
-            />
-          </FormElement>
+        {(() => {
+          switch (title) {
+            case 'profile':
+              return <Profile />
+              break
+            case 'taxes':
+              return <TaxAndTerms />
+              break
+            case 'password':
+              return <ChangePassword />
+              break
+            case 'subscription':
+              return <Subscription />
+              break
 
-          <FormElement>
-            <Label htmlFor=''>Phone *</Label>
-            <Input
-              type='number'
-              name='phone'
-              id='phone'
-              autoComplete='off'
-              placeholder=''
-            />
-          </FormElement>
-
-          <FormElement>
-            <Label htmlFor=''> Address *</Label>
-            <TextArea
-              type='text'
-              name='address'
-              id='address'
-              autoComplete='off'
-              placeholder=''
-              rows='2'
-            />
-          </FormElement>
-          <FormElement>
-            <Label htmlFor=''> </Label>
-            <FootNote>
-              Please note these information will be used in invoices. Please
-              check before saving them.
-            </FootNote>
-          </FormElement>
-        </Form>
-
-        <FormElement>
-          <Button label='success' clickHandle={() => clickHandle('')}>
-            Save My Profile
-          </Button>
-        </FormElement>
+            default:
+              return <Profile />
+          }
+        })()}
       </DetailSection>
     </Main>
   )
@@ -169,55 +120,4 @@ const DetailSection = styled.div`
   max-width: 1000px;
   overflow-x: auto;
   margin: 0 auto;
-`
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`
-const FormElement = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  /* gap: 2em; */
-  margin-bottom: 1em;
-  min-width: 500px;
-`
-
-const Label = styled.label`
-  flex: 1;
-`
-const Input = styled.input`
-  background-color: var(--white-color);
-  padding: 8px;
-  color: var(--black-color);
-  border: 1px solid var(--table-border-color);
-  border-radius: 4px;
-  width: 40%;
-  outline: none;
-  font-family: inherit;
-  &:focus {
-    box-shadow: var(--input-bs);
-  }
-  flex: 2;
-`
-
-const TextArea = styled.textarea`
-  background-color: var(--white-color);
-  padding: 8px;
-  color: var(--black-color);
-  border: 1px solid var(--table-border-color);
-  border-radius: 4px;
-  width: 40%;
-  outline: none;
-  font-family: inherit;
-  &:focus {
-    box-shadow: var(--input-bs);
-  }
-  flex: 2;
-`
-const FootNote = styled.div`
-  flex: 2;
-  font-size: 12px;
 `
