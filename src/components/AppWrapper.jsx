beee146e847ea6fb72482bf35781d664a94b636b
rahilansari261 <rahilansari261@gmail.com>
2023-05-24 23:10:46 +0530

    simpified code replaced

--- src/components/AppWrapper.jsx
+++ src/components/AppWrapper.jsx
@@ -17,7 +17,10 @@ export const AppWrapper = () => {
   const pathName = location.pathname.split("/");
   const [open, setOpen] = useState(false);
   const handleNav = (val) => {
-    if ((!open && val) || (!open && !val) || (open && !val) || (open && val)) {
+    // if ((!open && val) || (!open && !val) || (open && !val) || (open && val)) {
+    //   setOpen(!open);
+    // }
+    if (!open || val) {
       setOpen(!open);
     }
   };
